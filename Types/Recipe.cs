72e60a037757360aa8bc7b613072314f6d1b1f08
Michelle Mabuyo <michelle@apollographql.com>
2024-01-25 17:09:29 -0500

    lesson 9: contributing fields

---
+++ Types/Recipe.cs
@@ -0,0 +1,34 @@
+﻿using ApolloGraphQL.HotChocolate.Federation;
+
+namespace Odyssey.MusicMatcher;
+
+[Key("id")]
+public class Recipe
+{
+    [ID]
+    public string Id { get; }
+
+    [ReferenceResolver]
+    public static Recipe GetRecipeById(string id)
+    {
+        return new Recipe(id);
+    }
+
+    [GraphQLDescription(
+        "A list of recommended playlists for this particular recipe. Returns 1 to 3 playlists."
+    )]
+    public List<Playlist> RecommendedPlaylists()
+    {
+        return new List<Playlist>
+        {
+            new Playlist("1", "GraphQL Groovin'"),
+            new Playlist("2", "Graph Explorer Jams"),
+            new Playlist("3", "Interpretive GraphQL Dance")
+        };
+    }
+
+    public Recipe(string id)
+    {
+        Id = id;
+    }
+}
