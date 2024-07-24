e18d5a5f434b9c096fbf6cb2ede13d07db3053c2
Michelle Mabuyo <michelle@apollographql.com>
2024-07-24 12:31:56 -0400

    lesson 10: using requires external directives

--- Types/Recipe.cs
+++ Types/Recipe.cs
@@ -1,5 +1,6 @@
 using HotChocolate.ApolloFederation.Resolvers;
 using HotChocolate.ApolloFederation.Types;
+using SpotifyWeb;
 
 namespace Odyssey.MusicMatcher;
 
@@ -9,27 +10,38 @@ public class Recipe
     [Key]
     public string Id { get; }
 
+    [External]
+    public string? Name { get; }
+
     [ReferenceResolver]
-    public static Recipe GetRecipeById(string id)
+    public static Recipe GetRecipeById(string id, string? name)
     {
-        return new Recipe(id);
+        return new Recipe(id, name);
     }
 
     [GraphQLDescription(
         "A list of recommended playlists for this particular recipe. Returns 1 to 3 playlists."
     )]
-    public List<Playlist> RecommendedPlaylists()
+    [Requires("name")]
+    public async Task<List<Playlist>> RecommendedPlaylists(SpotifyService spotifyService)
     {
-        return new List<Playlist>
-        {
-            new Playlist("1", "GraphQL Groovin'"),
-            new Playlist("2", "Graph Explorer Jams"),
-            new Playlist("3", "Interpretive GraphQL Dance")
-        };
+        var response = await spotifyService.SearchAsync(
+            this.Name,
+            new List<SearchType> { SearchType.Playlist },
+            3,
+            0,
+            null
+        );
+
+        return response.Playlists.Items.Select(item => new Playlist(item)).ToList();
     }
 
-    public Recipe(string id)
+    public Recipe(string id, string? name)
     {
         Id = id;
+        if (name != null)
+        {
+            Name = name;
+        }
     }
 }
